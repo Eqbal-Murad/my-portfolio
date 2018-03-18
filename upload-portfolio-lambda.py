c1cc537164b57ffbbff2b4f8f8f0fd2084a3cbec
Eqbal Murad <eqbal.m@gmail.com>
2018-03-18 20:21:06 +1100

    Added lambda code to download build and push to s3

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,17 @@
+import boto3
+import StringIO
+import zipfile
+import mimetypes
+
+s3 = boto3.resource('s3')
+portfolio_bucket = s3.Bucket('about.eqbalmurad.com')
+build_bucket = s3.Bucket('portfolio-build.eqbalmurad.com')
+
+portfolip_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolip_zip)
+
+with zipfile.ZipFile(portfolip_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj,nm,ExtraArgs={'ContentType':mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
